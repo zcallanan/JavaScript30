849798fbf4ef9c966eb2e97e8c4f985e84bb6a8c
Zach Callanan <caveofmonsters@gmail.com>
2020-06-12 12:55:11 +0200

    completed

--- 04 - Array Cardio Day 1/arraycardio.js
+++ 04 - Array Cardio Day 1/arraycardio.js
@@ -22,25 +22,54 @@
 
     // Array.prototype.filter()
     // 1. Filter the list of inventors for those who were born in the 1500's
+    const fifteen = inventors.filter(inventor => (inventor.year >= 1500 && inventor.year < 1600));
+    console.table(fifteen);
 
     // Array.prototype.map()
     // 2. Give us an array of the inventors first and last names
+    const names = inventors.map(inventor => `${inventor.first} ${inventor.last}`);
+    console.log(names);
 
     // Array.prototype.sort()
     // 3. Sort the inventors by birthdate, oldest to youngest
+    const ordered = inventors.sort((a, b) => a.year - b.year);
+    console.table(ordered);
 
     // Array.prototype.reduce()
     // 4. How many years did all the inventors live all together?
+    const reduced = inventors.reduce((sum, inventor) => sum + (inventor.passed - inventor.year), 0);
+    console.log(reduced);
 
     // 5. Sort the inventors by years lived
+    const yearsLived = inventors.sort((a, b) => (a.passed - a.year) - (b.passed - b.year));
+    console.table(yearsLived);
 
     // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
     // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+    // const links = Array.from(document.querySelectorAll('.mw-category-group a'));
+    // array = links.map(link => link.title).filter(name => name.includes('de'))
+    // console.log(array);
 
 
     // 7. sort Exercise
     // Sort the people alphabetically by last name
+    const alpha = people.sort((a,b) => {
+      const [aLast, aFirst] = a.split(', ');
+      const [bLast, bFirst] = b.split(', ');
+      [aLast, aFirst] - [bLast, bFirst]
+      });
+    console.log(alpha);
 
     // 8. Reduce Exercise
     // Sum up the instances of each of these
     const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+    counted = data.reduce((cars, value) => {
+      if (value in cars) {
+        cars[value]++
+      }
+      else {
+        cars[value] = 1
+      }
+      return cars
+    }, {});
