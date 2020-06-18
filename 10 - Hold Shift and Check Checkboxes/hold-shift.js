b101b5fc1682a89d04e617506c5cf7d3cfaf7d75
Zach Callanan <caveofmonsters@gmail.com>
2020-06-19 00:16:41 +0200

    commented

--- 10 - Hold Shift and Check Checkboxes/hold-shift.js
+++ 10 - Hold Shift and Check Checkboxes/hold-shift.js
@@ -5,14 +5,15 @@ const handleCheck = (e) => {
   let inBetween = false;
   if (e.shiftKey === true && e.target.checked)
     inputs.forEach(checkbox => {
+      // if most recently checked box or the previously checked box are checked
       if (checkbox === e.target || checkbox === lastChecked) {
         inBetween = !inBetween;
       }
-      if (inBetween === true) {
+      if (inBetween === true) { // check the boxes in beetween previous and latest (e.target)
         checkbox.checked = true;
       }
     })
-  lastChecked = e.target
+  lastChecked = e.target // save most recently checked box
 }
 
 inputs.forEach(element => addEventListener('click', handleCheck));
