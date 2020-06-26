7607cce1b09efa1aa6c0c8a3fe8304551ccd2a8e
Zach Callanan <caveofmonsters@gmail.com>
2020-06-26 11:24:58 +0200

    completed

--- 15 - LocalStorage/local-storage.js
+++ 15 - LocalStorage/local-storage.js
@@ -1,3 +1,42 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
-const items = [];
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+const addItem = (e) => {
+  e.preventDefault();
+
+  const text = (e.target.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  }
+  items.push(item);
+  e.target.reset();
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));  // local storage only takes text
+}
+
+const populateList = (plates = [], platesList) => {
+  platesList.innerHTML = plates.map((plate, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''}/>
+        <label for="item${i}">${plate.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+const toggleDone = (e) => {
+  if (!e.target.matches('input')) return; // skip unless input, next if...
+  const el = e.target;
+  items[el.dataset.index].done = !items[el.dataset.index].done
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
+
